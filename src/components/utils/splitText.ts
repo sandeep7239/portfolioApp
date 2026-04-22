f50884eab9d7f4810eec58d9eba876f1501e04ef
sandeep7239 <sandeep723992singh@gmail.com>
2026-04-22 17:31:35 +0530

    handle gsap version issue

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -1,7 +1,6 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
-import { SplitText } from "gsap-trial/SplitText";
+import { ScrollSmoother, SplitText } from "gsap/all";
 
 interface ParaElement extends HTMLElement {
   anim?: gsap.core.Animation;
